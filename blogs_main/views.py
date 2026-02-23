e0081132ffede6cb72aa849dcd0bc52517a7f618
project <2k23.cs2312094@gmail.com>
2026-02-23 22:50:50 +0530

    second commit

--- blogs_main/views.py
+++ blogs_main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
+
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    feature_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('-updated_at')
+    post = Blog.objects.filter(is_featured=False, status="Published").order_by('-updated_at')
+    context = {
+            'categories': categories,
+            'feature_posts': feature_posts,
+            'post': post,
+    }
+    return render(request, 'home.html', context)
